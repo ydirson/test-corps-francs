ac322c8f2e88f705c0acfe9fd386d77271dbfebf
Yann Dirson <ydirson@free.fr>
2025-07-20 19:37:30 +0200

    Switch army book to JSON5, use comments
    Slightly better, but not 100% satisfactory yet.

--- src/data/validate.py
+++ src/data/validate.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python3
 
 import json
+import json5
 import jsonschema
 
 with (open("src/data/rules-schema.json") as schema_fd,
-      open("src/data/rules.json") as data_fd):
+      open("src/data/rules.json5") as data_fd):
     schema = json.load(schema_fd)
-    data = json.load(data_fd)
+    data = json5.load(data_fd)
 
 jsonschema.validate(instance=data, schema=schema)
