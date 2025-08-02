9b3ccf08e8b27329eb88b286de31bd8d0528e5b1
Yann Dirson <ydirson@free.fr>
2025-08-02 17:21:37 +0200

    Convert to TOML
    Done by:
    - dumping using tomlkit
    - manually transferring comments
    
    And still validate using the json schema, nice :)

--- src/data/validate.py
+++ src/data/validate.py
@@ -3,15 +3,15 @@
 import json
 import os
 import sys
+import tomllib
 
-import json5
 import jsonschema
 
 mydir = os.path.dirname(sys.argv[0])
 
 with (open(os.path.join(mydir, "rules-schema.json")) as schema_fd,
-      open(os.path.join(mydir, "rules.json5")) as data_fd):
+      open(os.path.join(mydir, "rules.toml"), "rb") as data_fd):
     schema = json.load(schema_fd)
-    data = json5.load(data_fd)
+    data = tomllib.load(data_fd)
 
 jsonschema.validate(instance=data, schema=schema)
